cc205c1229eb16f7f4a9545a8ce46a11e5b2a427
Bogarh451 <contacto.eduardo.rosas@gmail.com>
2020-08-08 10:12:38 -0500

    v1.0
    v1.0

--- test/app/appTestR.js
+++ test/app/appTestR.js
@@ -1,8 +1,10 @@
 
 const ansiedadSS = sessionStorage.getItem("ansiedad");
 const ayudaSS = sessionStorage.getItem("ayuda");
+const tipoSS = sessionStorage.getItem("tipo");
 
 document.getElementById("tipoAnsiedad").innerHTML = ansiedadSS;
 document.getElementById("validaAyuda").innerHTML = ayudaSS;
+document.getElementById("ansiedadExp").innerHTML = tipoSS;
 
 
