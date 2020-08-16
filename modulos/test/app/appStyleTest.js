35f7c40cea3acf7369321acbde57fc21814f109d
Bogarh451 <contacto.eduardo.rosas@gmail.com>
2020-08-16 17:23:29 -0500

    Beta v1.0
    Beta v1.0

---
+++ modulos/test/app/appStyleTest.js
@@ -0,0 +1,228 @@
+
+
+let contenedor = document.getElementById("contenedorAnima");
+
+/*let anteriorBtn1 = document.getElementById("anterior1");*/
+let siguienteBtn1 = document.getElementById("siguiente1");
+
+let anteriorBtn2 = document.getElementById("anterior2");
+let siguienteBtn2 = document.getElementById("siguiente2");
+
+let anteriorBtn3 = document.getElementById("anterior3");
+let siguienteBtn3 = document.getElementById("siguiente3");
+
+let anteriorBtn4 = document.getElementById("anterior4");
+let siguienteBtn4 = document.getElementById("siguiente4");
+
+let anteriorBtn5 = document.getElementById("anterior5");
+let siguienteBtn5 = document.getElementById("siguiente5");
+
+let anteriorBtn6 = document.getElementById("anterior6");
+let siguienteBtn6 = document.getElementById("siguiente6");
+
+let anteriorBtn7 = document.getElementById("anterior7");
+let siguienteBtn7 = document.getElementById("siguiente7");
+
+let anteriorBtn8 = document.getElementById("anterior8");
+let siguienteBtn8 = document.getElementById("siguiente8");
+
+let anteriorBtn9 = document.getElementById("anterior9");
+let siguienteBtn9 = document.getElementById("siguiente9");
+
+let anteriorBtn10 = document.getElementById("anterior10");
+let siguienteBtn10 = document.getElementById("siguiente10");
+
+let anteriorBtn11 = document.getElementById("anterior11");
+let siguienteBtn11 = document.getElementById("siguiente11");
+
+let anteriorBtn12 = document.getElementById("anterior12");
+let siguienteBtn12 = document.getElementById("siguiente12");
+
+let anteriorBtn13 = document.getElementById("anterior13");
+let siguienteBtn13 = document.getElementById("siguiente13");
+
+let anteriorBtn14 = document.getElementById("anterior14");
+/*let siguienteBtn14 = document.getElementById("siguiente14");*/
+
+/*BOTON UNO*/ 
+
+siguienteBtn1.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente1");
+    
+});
+/*
+anteriorBtn1.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior1");
+
+});
+*/
+/*BOTON DOS*/
+
+siguienteBtn2.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente2");
+    
+});
+
+anteriorBtn2.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior2");
+    
+});
+
+/*BOTON TRES*/
+
+siguienteBtn3.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente3");
+    
+});
+
+anteriorBtn3.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior3");
+    
+});
+
+/*BOTON CUATRO*/
+
+siguienteBtn4.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente4");
+    
+});
+
+anteriorBtn4.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior4");
+    
+});
+
+/*BOTON CINCO*/
+
+siguienteBtn5.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente5");
+    
+});
+
+anteriorBtn5.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior5");
+    
+});
+
+
+/*BOTON SEIS*/
+
+siguienteBtn6.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente6");
+    
+});
+
+anteriorBtn6.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior6");
+    
+});
+
+/*BOTON SIETE*/
+
+siguienteBtn7.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente7");
+    
+});
+
+anteriorBtn7.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior7");
+    
+});
+
+/*BOTON OCHO*/
+
+siguienteBtn8.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente8");
+    
+});
+
+anteriorBtn8.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior8");
+    
+});
+
+/*BOTON NUEVE*/
+
+siguienteBtn9.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente9");
+    
+});
+
+anteriorBtn9.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior9");
+    
+});
+
+/*BOTON DIEZ*/
+
+siguienteBtn10.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente10");
+    
+});
+
+anteriorBtn10.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior10");
+    
+});
+
+
+/*BOTON ONCE*/
+
+siguienteBtn11.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente11");
+    
+});
+
+anteriorBtn11.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior11");
+    
+});
+
+
+/*BOTON DOCE*/
+
+siguienteBtn12.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente12");
+    
+});
+
+anteriorBtn12.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior12");
+    
+});
+
+
+/*BOTON TRECE*/
+
+siguienteBtn13.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente13");
+    
+});
+
+anteriorBtn13.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior13");
+    
+});
+
+
+/*BOTON CATORCE*/
+/*
+siguienteBtn14.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollSiguiente14");
+    
+});
+*/
+anteriorBtn14.addEventListener("click", function(){
+    contenedor.setAttribute("class", "scrollAnterior14");
+    
+});
+
+/*APARICION*/
+
+let inicaTest = document.getElementById("inicarTest");
+let start = document.getElementById("testyIns");
+
+inicaTest.addEventListener("click",function(){
+    start.setAttribute("class", "desapareceIns");
+});
+
---
+++ modulos/test/app/appTest.js
@@ -0,0 +1,121 @@
+/* Juega con el */
+function valorInput(){
+  var actualValor = document.getElementById("juegaInput").value;
+  
+  switch (actualValor){
+    case "0":
+      document.getElementById("muestraValor").innerHTML = actualValor + " <br>Ausente";
+    break;
+
+    case "1":
+      document.getElementById("muestraValor").innerHTML = actualValor + " <br>Intensidad Leve";
+    break;
+
+    case "2":
+      document.getElementById("muestraValor").innerHTML = actualValor + "<br>Intensidad Moderada";
+    break;
+
+    case "3":
+      document.getElementById("muestraValor").innerHTML =  actualValor + "<br>Intensidad Grave";
+    break;
+
+    case "4":
+      document.getElementById("muestraValor").innerHTML = actualValor + "<br>Totalmente Incapacitado";
+    break;
+
+  }
+}
+
+
+
+
+
+
+
+
+/*No animation*/
+
+
+
+let boton = document.getElementById("btnResultado");
+
+boton.addEventListener("click", function(){
+  var preg1 = document.getElementById("test1").value;
+  var num1= Number(preg1);
+  var preg2 = document.getElementById("test2").value;
+  var num2= Number(preg2);
+  var preg3 = document.getElementById("test3").value;
+  var num3= Number(preg3);
+
+  var preg4 = document.getElementById("test4").value;
+  var num4= Number(preg4);
+  var preg5 = document.getElementById("test5").value;
+  var num5= Number(preg5);
+  var preg6 = document.getElementById("test6").value;
+  var num6= Number(preg6);
+
+  var preg7 = document.getElementById("test7").value;
+  var num7= Number(preg7);
+  var preg8 = document.getElementById("test8").value;
+  var num8= Number(preg8);
+  var preg9 = document.getElementById("test9").value;
+  var num9= Number(preg9);
+
+
+  var preg10 = document.getElementById("test10").value;
+  var num10= Number(preg10);
+  var preg11 = document.getElementById("test11").value;
+  var num11= Number(preg11);
+  var preg12 = document.getElementById("test12").value;
+  var num12= Number(preg12);
+
+  var preg13 = document.getElementById("test13").value;
+  var num13= Number(preg13);
+  var preg14 = document.getElementById("test14").value;
+  var num14= Number(preg14);
+  
+var sumaTodo = 0, sumaCognitiva, sumaSomatica, esTipo, puedoAyudar;
+var ansiedadLeve, ansiedadGrave, ansiedadAusente;
+
+  sumaTodo = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10 + num11 + num12 + num13 + num14;
+  sumaCognitiva = num1 + num2 + num3 + num4 + num5 + num6 + num14;
+  sumaSomatica = num7 + num8 + num9 + num10 + num11 + num12 + num13 + num14;
+
+   
+  if(sumaTodo <= 5){
+
+    /*alert(sumaTodo+" "+sumaCognitiva);*/
+    esTipo = "ANSIEDAD AUSENTE";
+    puedoAyudar = "SI CREES QUE EL TEST NO REFLEJA COMO TE SIENTES";
+    ansiedadAusente = "a"
+
+    sessionStorage.setItem("ansiedad", esTipo);
+    sessionStorage.setItem("ayuda", puedoAyudar);
+    sessionStorage.setItem("tipo", ansiedadAusente);
+
+   }
+
+   if(sumaTodo >= 6 && sumaTodo < 15 ){
+    /*alert(sumaTodo+" "+sumaCognitiva); */
+    esTipo = "TU ANSIEDAD ES LEVE";
+    puedoAyudar = "DEFINITIVAMENTE PUEDO AYUDARTE CON ESO";
+    ansiedadLeve = "b";
+
+    sessionStorage.setItem("ansiedad", esTipo);
+    sessionStorage.setItem("ayuda", puedoAyudar);
+    sessionStorage.setItem("tipo", ansiedadLeve);
+
+   } 
+   if(sumaTodo >= 15){
+     
+   /* alert(sumaTodo+" "+sumaCognitiva); */
+    esTipo = "TU ANSIEDAD REQUIERE TRATAMIENTO";
+    puedoAyudar = "DEFINITIVAMENTE PUEDO AYUDARTE CON ESO";
+    ansiedadGrave = "c";
+
+    sessionStorage.setItem("ansiedad", esTipo);
+    sessionStorage.setItem("ayuda", puedoAyudar);
+    sessionStorage.setItem("tipo", ansiedadGrave);
+   }
+  
+});
